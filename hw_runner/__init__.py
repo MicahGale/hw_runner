92e986376d07a500d0c233d3d6cd6b0d2dfcb86d
Micah Gale <mg@gale.engineer>
2023-10-01 18:09:49 -0500

    Added command line arguments.

--- hw_runner/__init__.py
+++ hw_runner/__init__.py
@@ -1,3 +1,26 @@
+import argparse
 
-def main():
-    print("it works")
+from .runner import Runner
+
+
+def parse_arguments(course_name):
+    parser = argparse.ArgumentParser(
+        prog=course_name, description=f"A homework runner for the class: {course_name}"
+    )
+    parser.add_argument(
+        "assignment",
+        nargs="?",
+        help="The assignment number to run, or 'all'",
+        default="all",
+    )
+    parser.add_argument(
+        "question",
+        nargs="?",
+        help="The question number to run. Defaults to all.",
+        default="all",
+    )
+    return parser.parse_args()
+
+
+def main(course_name):
+    arguments = parse_arguments(course_name)
